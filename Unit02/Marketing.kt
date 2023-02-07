2532c06a25d6a7520f98b0f37515d15f3ae3f50d
JosueMaciass <123098854+JosueMaciass@users.noreply.github.com>
2023-02-07 14:13:14 -0600

    this is markerking for Unit02

---
+++ Unit02/Marketing.kt
@@ -0,0 +1,16 @@
+//Josue Macias
+//02/07/2023
+
+fun main() 
+{
+   var friendsCount = 500
+    
+   if (friendsCount >= 500)
+    {
+        println("Adopt a Dog Today!!")
+    }
+    else
+        {
+            println("Buy Cat Food At 20% Off!!!")
+        }
+}
