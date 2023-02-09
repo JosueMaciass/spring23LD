ad3975d6497b5ec3b9b8287f4e04d92bafa64edd
JosueMaciass <123098854+JosueMaciass@users.noreply.github.com>
2023-02-09 09:18:29 -0600

    m

---
+++ Unit02/Salary.kt
@@ -0,0 +1,9 @@
+//Josue Macias
+//02/09/2023
+fun main() {
+   val name = "Josue";
+   val age = 20;
+   val annualPay = 100000;
+   println("My name is $name, my age is $age and I hope to earn $annualPay per year")
+   
+}
