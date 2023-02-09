8ac9e81766fe69963dba0d7a345639dad75aaf07
JosueMaciass <123098854+JosueMaciass@users.noreply.github.com>
2023-02-09 09:30:15 -0600

    g

---
+++ Unit02/star.kt
@@ -0,0 +1,12 @@
+//Josue Macias
+//02/09/2023
+
+fun main() {
+    println("   *")
+    println("  ***")
+    println(" *****")
+    println("*******")
+    println(" *****")
+    println("  ***")
+    println("   *")
+}
