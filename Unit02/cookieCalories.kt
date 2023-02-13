200982bc44e908c96f98136ba3c72895bc0b5a61
JosueMaciass <123098854+JosueMaciass@users.noreply.github.com>
2023-02-12 21:48:31 -0600

    h

---
+++ Unit02/cookieCalories.kt
@@ -0,0 +1,8 @@
+import java.util.Scanner  
+fun main(args: Array<String>) {  
+    val read = Scanner(System.`in`)  
+    println("Enter number of cookies eaten:")  
+    var numOfCookies= read.nextInt()  
+    val total = numOfCookies* 75
+    println("Your calorie intake was: "+ total)  
+} 
