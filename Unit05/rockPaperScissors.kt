7d2cc0c9ae031caab830f50ea5a6fb8daa3d4c4d
JosueMaciass <123098854+JosueMaciass@users.noreply.github.com>
2023-05-09 10:27:53 -0500

    .

--- Unit05/rockPaperScissors.kt
+++ Unit05/rockPaperScissors.kt
@@ -1,8 +1,12 @@
+// Josue Macias
+// 4/21/2023
+
+
 import kotlin.random.Random
 
 fun main(args: Array<String>) {
     var playAgain = true
-
+        // asking if the person if they want to play 1 = yes and 2 = no
     while(playAgain) {
         print("Do you want to play? (1 = Y /2 = N)")
         var play = readLine()?.toIntOrNull()
@@ -11,7 +15,7 @@ fun main(args: Array<String>) {
         } else if (play == 2) {
             playAgain = false
         }
-        if (playAgain) {
+        if (playAgain) {        //showing them how to play
             println("These are the rules of the game")
             println("Rock is 1")
             println("Paper is 2")
@@ -20,9 +24,10 @@ fun main(args: Array<String>) {
             println("What is your choice?")
             var playerChoice: Int? = readLine()?.toIntOrNull()
 
-            val cpu = Random.nextInt(1, 4)
+            val cpu = Random.nextInt(1, 4) //telling the computer to pick a number from 1 - 4 
             println("Computer picks $cpu")
 
+            // Doing the logic to see if the player won or if the computer won
             if ((playerChoice == 1 && cpu == 3) || (playerChoice == 2 && cpu == 1) || (playerChoice == 3 && cpu == 2))
                 println("YOU WIN!!!")
             else
@@ -32,7 +37,7 @@ fun main(args: Array<String>) {
                     if ((playerChoice == 1 && cpu == 1) || (playerChoice == 2 && cpu == 2) || (playerChoice == 3 && cpu == 3))
                         println("You Tie(---)")
                     else println("--->TRY AGAIN<---")
-        }
+        }   //asking if they want to play again
         print("Do you want to play again? (1 = Y /2 = N)")
        play = readLine()?.toIntOrNull()
         if (play == 1) {
